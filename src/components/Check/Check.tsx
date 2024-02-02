815a3766b09e38facd8bb3bf8839ce371424a166
blobby-bobby <bulle.ouvrard@gmail.com>
2024-02-02 12:26:10 +0100

    Check, added background color change on selected items

--- src/components/Check/Check.tsx
+++ src/components/Check/Check.tsx
@@ -1,16 +1,29 @@
-import { FunctionComponent } from "react"
+import { FunctionComponent, useState } from "react"
 import { optionProps } from "../../types/types";
 import styles from './check.module.scss'
 
 export const Check: FunctionComponent<optionProps> = (props) => {
+
+  const [checkedItems, setCheckedItems] = useState<string[]>([]);
+
+  const handleCheckboxChange = (value: string) => {
+    setCheckedItems((prevCheckedItems) => {
+      if (prevCheckedItems.includes(value)) {
+        return prevCheckedItems.filter(item => item !== value);
+      } else {
+        return [...prevCheckedItems, value];
+      }
+    });
+  };
+
   return (
-  <ul className={styles.checklist}>
-    {props.options.map(option => (
-      <li key={option.value} className={styles["checklist__item"]}>
-        <input type="checkbox" id={option.label} name={option.label} className={styles.checkbox} />
-        <label htmlFor={option.label}>{option.label}</label>
-      </li>
-    ))}
-  </ul>  
+    <ul className={styles.checklist}>
+      {props.options.map(option => (
+        <li key={option.value} className={`${styles["checklist__item"]} ${checkedItems.includes(option.label) ? styles.checked : ""}`}>
+          <input type="checkbox" id={option.label} name={option.label} className={styles.checkbox} onClick={() => handleCheckboxChange(option.label)} />
+          <label htmlFor={option.label}>{option.label}</label>
+        </li>
+      ))}
+    </ul>  
   )
 }
